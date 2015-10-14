99a3e321d0f91f6f24868a56149f21e942a9827a
lanou3g <1213850155@qq.com>
2015-10-14 11:32:13 +0800

    no message

---
+++ Homework_QQ/Homework_QQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework_QQ
+//
+//  Created by lanou3g on 15/8/21.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Homework_QQ/Homework_QQ/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  Homework_QQ
+//
+//  Created by lanou3g on 15/8/21.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+- (IBAction)Login:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *QQ_number;
+@property (weak, nonatomic) IBOutlet UITextField *QQ_pwd;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)Login:(id)sender {
+    NSLog(@"%@",self.QQ_number.text);
+    NSLog(@"%@",self.QQ_pwd.text);
+    [self.view endEditing:YES];
+}
+@end
